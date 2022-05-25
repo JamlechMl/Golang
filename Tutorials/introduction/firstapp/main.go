b6e3d206848584194fdd4628acb5fc43e912f9bf
JamlechMl <jamlechmukavana601@gmail.com>
2022-05-25 10:47:10 +0300

    first commit

---
+++ Tutorials/introduction/firstapp/main.go
@@ -0,0 +1,15 @@
+package main
+
+var (
+	actorName    string = "jamlech mukavana"
+	companion    string = "leyla martin"
+	doctorNumber int    = 4
+)
+
+func main() {
+	// var i int
+	// i = 24
+	// i := 42
+	// fmt.Printf("%v, %T", i, i)
+
+}
